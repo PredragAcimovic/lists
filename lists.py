4422080f81e743e2395ee85d38bfe27c74f991f9
Predrag <pedjaacimovic44@gmail.com>
2023-12-18 18:57:06 +0100

    lists

--- lists.py
+++ lists.py
@@ -45,3 +45,66 @@ motorcycles = ['honda', 'yamaha', 'suzuki']
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
 #removing elements from a list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[2]
+print(motorcycles)
+#removing an item using the pop() method
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motrtcycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motrtcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop()
+print(f"The last motorcycles i owned was a {last_owned.title()}.")
+#popping items from any position in a list
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycles i owned was a {first_owned.title()}.")
+#removing an item by value
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki','ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+#3-4
+guests = ['marko teovanovic', 'marija acimovic', 'jelena vujic']
+print(guests)
+message = f"Hello, will you, {guests[2].title()} come too dinner, tonight at my place?"
+print(message)
+#3-5
+is_unable = guests.pop(1)
+print(f"{is_unable.title()}, is not able to come to dinner.")
+
+guests.insert(1, 'petar petrovic')
+message = f"Hello, will you, {guests[0].title(), guests[1].title(), guests[2].title()} come too dinner, tonight at my place?"
+print(message)
+guests.insert(0, 'dragan acimovic')
+guests.insert(1, 'dragana acimovic')
+guests.append('nikola vujic')
+message = f"Hello, will you, {guests[0].title()}, come to dinner, tonight at my place?"
+print(message)
+message = f"Hello, will you, {guests[1].title()}, come to dinner, tonight at my place?"
+print(message)
+message = f"Hello, will you, {guests[2].title()}, come to dinner, tonight at my place?"
+print(message)
+message = f"Hello, will you, {guests[3].title()}, come to dinner, tonight at my place?"
+print(message)
+message = f"Hello, will you {guests[4].title()}, come to dinner, tonight at my place?"
+print(message)
+message = f"Hello, will you {guests[5].title()}, come to dinner, tonight at my place?"
+print(message)
+message = f"Hello my guests, im sorry, but i have only two empty space for dinner, i can invite only two of you."
+print(message)
+message = f"Hello, {guests[2].title()}, im sorry but i cant invite you."
+print(message)
+message = f"Hello, {guests[3].title()}, im sorry but i cant invite you."
+print(message)
+message = f"Hello, {guests[4].title()}, im sorry but i cant invite you."
+print(message)
+message = f"Hello, {guests[5].title()}, im sorry but i cant invite you."
+print(message)
